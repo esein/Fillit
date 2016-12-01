c68b4ec112bce7c4c8d35b6e67eb411308167ddc
Gaetan CADIOU <gcadiou@e1r12p19.42.fr>
2016-12-01 09:55:03 +0100

    recup_piece op

--- recup_piece.c
+++ recup_piece.c
@@ -6,34 +6,84 @@
 /*   By: gcadiou <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 02:26:46 by gcadiou           #+#    #+#             */
-/*   Updated: 2016/11/30 07:10:58 by gcadiou          ###   ########.fr       */
+/*   Updated: 2016/12/01 09:53:52 by gcadiou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include "libft.h"
 
 #define BUF_SIZE 21
+#define L_SIZE 10
+#define I_SIZE 5
 
-int		file_error(void)
+static void	*file_error(char c, char **tab)
 {
-	putstr("error");
+	ft_memdel((void **)tab);
+	ft_putchar(c);
+	ft_putendl("error");
 	return (0);
 }
 
-int		check(int argc, char **argv)
+char		**check_file(int fd)
 {
-	char	**tab[26][26];
+	char	**tab;
 	size_t	ret;
-	int		fd;
 	char	buf[BUF_SIZE + 1];
+	size_t	l = 0;
+	size_t	i= 0;
+	size_t	count = 0;
 
-		if (argc != 2)
-		return 0;
-	fd = open(argv[1],O_RDONLY);
+	tab = ft_mal_double_tab(tab, L_SIZE, I_SIZE);
+	while (count < L_SIZE)
+	{
+		ft_memset(tab[count], '0', I_SIZE);
+		count++;
+	}
 	while ((ret = read(fd, buf, BUF_SIZE)))
 	{
-		if (buf[ret] != '\n')
-			return (file_error());
-		
+		if (ret == 20)
+		{
+			if((ret = read(fd, buf, BUF_SIZE)))
+				return (file_error('2', tab));
+			count = 0;
+		}
+		else
+			if (buf[ret-1] != '\n' || ret != 21)
+				return (file_error('1', tab));
+		while (l < 5)
+		{
+			i = 0;
+			while (i < 4)
+			{
+				tab[l][i] = buf[i];
+				i++;
+			}
+			if (buf[i] != '\n')
+				return file_error('3',tab);
+			tab[l][i] = buf[i];
+			l++;
+		}
 	}
+	if (count)
+		return (file_error('4',tab));
+	return (tab);
+}
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	**tab;
+
+	if (argc != 2)
+	{
+		ft_putendl("usage: ./fillit file_test");
+		return 0;
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (!(tab = check_file(fd)))
+			return (0);
+	ft_print_double_tab(tab, L_SIZE, I_SIZE);
+}
